46498fd7f641cae8880f8be8696f6f5cebbed2d4
habibullo071 <habibulloqodirov2006@gmail.com>
2024-03-27 11:27:33 +0300

    dhbbdhb

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <h1>App</h1>
+      <Footer/>
     </div>
   );
 }
