288a4fc82e1d8adcf0fcbf0d92cb6cd5353a65dc
habibullo071 <habibulloqodirov2006@gmail.com>
2024-03-27 15:42:04 +0300

    jjfvjj

--- src/components/Trate/Trate.jsx
+++ src/components/Trate/Trate.jsx
@@ -2,17 +2,20 @@ import React from 'react'
 import './Trate.scss'
 import trade from '../../img/IMAGE.png'
 import trade1 from '../../img/Frame1.png'
+import trade2 from '../../img/IMAGE .png'
 
 function Trate() {
     return (
         <div className='trade'>
-            Trade
+            <div className="TRADE">
+            TRADE
+            </div>
             <div className="trade__main">
                 <div className="left">
                     <h2>Buy, sell and trade crypto with confidence</h2>
                     <ul>
                         <li>
-                            <img src={trade1} alt="" />
+                            <img src={trade1} alt=""/>
                             <b>Simple buy & sell options</b>
                             <p>Buy and sell cryptocurrency quickly and easily.</p>
                         </li>
@@ -37,6 +40,37 @@ function Trate() {
                     <img src={trade} alt="" />
                 </div>
             </div>
+            <div className="wallet">
+            WALLET
+            </div>
+            <ul className='trade__button'>
+                <li className='left'><img src={trade2} alt="" /></li>
+                <li className='right'>
+                    <h2>Simple and secure wallet services</h2>
+                    <ul>
+                        <li>
+                            <img src={trade1} alt="" />
+                            <b>Simple buy & sell options</b>
+                            <p>We use multi-signature technology to securely store your assets.</p>
+                        </li>
+                        <li>
+                            <img src={trade1} alt="" />
+                            <b>Simple buy & sell options</b>
+                            <p>We use multi-signature technology to securely store your assets.</p>
+                        </li>
+                        <li>
+                            <img src={trade1} alt="" />
+                            <b>Simple buy & sell options</b>
+                            <p>We use multi-signature technology to securely store your assets.</p>
+                        </li>
+                        <li>
+                            <img src={trade1} alt="" />
+                            <b>Simple buy & sell options</b>
+                            <p>We use multi-signature technology to securely store your assets.</p>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
         </div>
     )
 }
