23c2e29ecf865924d72394514bd4aa66fe249ef5
habibullo071 <habibulloqodirov2006@gmail.com>
2024-03-27 12:08:54 +0300

    hbhbhvf

--- src/components/Why/Why.jsx
+++ src/components/Why/Why.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import './Why.scss'
+import why from '../../img/Frame.png'
 
 function Why() {
     return (
-        <div>
-            <h1>Why</h1>
+        <div className='Why'> 
+            <h2>Why BTC Markets?</h2>
+            <ul>
+                <li>
+                    <img src={why} alt="" />
+                    <b>Security</b>
+                    <p>Multiple layers of account security, 2FA, address whitelisting, Biometrics.</p>
+                </li>
+                <li>
+                    <img src={why} alt="" />
+                    <b>Security</b>
+                    <p>Multiple layers of account security, 2FA, address whitelisting, Biometrics.</p>
+                </li>
+                <li>
+                    <img src={why} alt="" />
+                    <b>Security</b>
+                    <p>Multiple layers of account security, 2FA, address whitelisting, Biometrics.</p>
+                </li>
+            </ul>
         </div>
     )
 }
